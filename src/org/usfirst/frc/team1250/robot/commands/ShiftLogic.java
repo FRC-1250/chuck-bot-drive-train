f405abdff154045d11a2fdd59f48b21a1d59761e
georgealami <galami20@hfa-dearborn.org>
2018-01-13 17:42:32 -0500

    Created Initial Shifter Logic

---
+++ src/org/usfirst/frc/team1250/robot/commands/ShiftLogic.java
@@ -0,0 +1,49 @@
+package org.usfirst.frc.team1250.robot.commands;
+import org.usfirst.frc.team1250.robot.Robot;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ShiftLogic extends Command {
+	
+	static int LeftVelocity=0;
+	static int RightVelocity = 0;
+	
+	
+    public ShiftLogic() {
+        // Use requires() here to declare subsystem dependencies
+         requires(Robot.m_drivetrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	
+    	RightVelocity = Robot.m_drivetrain.getRightRPM();
+    	LeftVelocity = Robot.m_drivetrain.getLeftRPM();
+    	
+    	
+    	
+    	
+    	
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
--- src/org/usfirst/frc/team1250/robot/subsystems/DriveTraine.java
+++ src/org/usfirst/frc/team1250/robot/subsystems/DriveTraine.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1250.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
@@ -15,22 +16,31 @@ import org.usfirst.frc.team1250.robot.commands.DriveTankWithJoystick;
  */
 public class DriveTraine extends Subsystem {
 
+	WPI_TalonSRX fLeftMotor = new WPI_TalonSRX(RobotMap._frontLeftMotor);
+	WPI_TalonSRX bLeftMotor = new WPI_TalonSRX(RobotMap._backLeftMotor);
+	WPI_TalonSRX fRightMotor = new WPI_TalonSRX(RobotMap._frontRightMotor);
+	WPI_TalonSRX bRightMotor = new WPI_TalonSRX(RobotMap._backRightMotor);
 	
 	
 	private SpeedController m_leftMotor
-	= new SpeedControllerGroup(new WPI_TalonSRX(RobotMap._frontLeftMotor), new WPI_TalonSRX(RobotMap._backLeftMotor));
+	= new SpeedControllerGroup(fLeftMotor, bLeftMotor);
 	
 	private SpeedController m_rightMotor
-	= new SpeedControllerGroup(new WPI_TalonSRX(RobotMap._frontRightMotor), new WPI_TalonSRX(RobotMap._backRightMotor));
+	= new SpeedControllerGroup(fRightMotor, bRightMotor);
 	
 	private DifferentialDrive m_drive
 	= new DifferentialDrive(m_leftMotor, m_rightMotor);
+	
+	 private Solenoid Solenoid0 = new Solenoid(1);
+
+
+	 private Solenoid Solenoid1 = new Solenoid(2);
 
 	
 	public DriveTraine() {
 		// Extends Subsystem class
-		super();
-		
+		super();				
+
 
 	}
 	
@@ -63,10 +73,45 @@ public class DriveTraine extends Subsystem {
 		drive(-joy.getY(), -joy.getThrottle());
 	}
 	
+	public void setShift(boolean shift_bool) {
+
+		if (shift_bool) {
+			
+			Solenoid0.set(true);
+			Solenoid1.set(true);	
+		}			
+		else{
+			Solenoid0.set(false);
+			Solenoid1.set(false);
+		}
+	}
+	
+	public void toggleShift() {
+		boolean curr_state = Solenoid0.get();
+		
+		Solenoid0.set(!curr_state);
+		Solenoid1.set(!curr_state);
+		
+	}
+	
+	public boolean getShift() {
+		return Solenoid0.get();
+	}
 	
 	public void log() {
 		
 	}
 	
+	public int getRightRPM() {
+		int velocity;
+		velocity = fRightMotor.getSelectedSensorVelocity(0);
+		return velocity;
+	}
+	
+	public int getLeftRPM() {
+		int velocity;
+		velocity = fLeftMotor.getSelectedSensorVelocity(0);
+		return velocity;
+	}
 }
 
