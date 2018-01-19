3228197338a207b9fdb9d2ec9b67ac9829e5fcc0
TarsLaptop <hfarobotics@hfa-dearborn.org>
2018-01-19 17:58:12 -0500

    x
    d

--- src/org/usfirst/frc/team1250/robot/Robot.java
+++ src/org/usfirst/frc/team1250/robot/Robot.java
@@ -5,7 +5,8 @@ import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import org.usfirst.frc.team1250.robot.RobotMap;
 
 import org.usfirst.frc.team1250.robot.subsystems.DriveTraine;
@@ -22,11 +23,18 @@ public class Robot extends IterativeRobot {
 	
 //OI ALWAYS LAST !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     public void robotInit() {
+    	
+//    	SmartDashboard.putData(m_drivetrain);
+//    	SmartDashboard.putData(m_claw);
+		
     	CameraServer.getInstance().startAutomaticCapture();
     	rb = new RobotMap();
     	m_drivetrain = new DriveTraine();
     	m_claw = new Claw();
     	m_oi = new OI();
+    	   	
+    	//SmartDashboard.putData(m_drivetrain);
+    	//SmartDashboard.putData(m_claw);
     }
 
 
